fad41e2c6bb874a3c5bde741ff91f7fcd9a72356
Drew Coker <drewrosecoker@gmail.com>
2023-02-28 19:09:57 -0500

    made comments easier to understand

--- python-wordle.py
+++ python-wordle.py
@@ -5,24 +5,39 @@ start_game = "Play Wordle\n"
 make_guess = "Enter your guess:\n"
 total_guesses = 6
 
-#player guess has two arguments - guess and answer
+
+#ANSI COLOR KEY 
+#\033 is the escape character that signals the beginning of an escape sequence.
+# 1 after the escape character sets the style to bold.
+# 30 sets the text color to black.
+# 31 sets the text color to red.
+# 32 sets the text color to green.
+# 33 sets the text color to yellow.
+# 42 sets the background color to green.
+# 43 sets the background color to yellow.
+# 47 sets the background color to white.
+#\033[0m resets the style and colors
+
+
+
 def player_guess(guess, answer):
-    #set wordle = empty array 
     wordle = []
-    #letters guessed to empty array
     letters_guessed = []
     for i, letter in enumerate(guess):
-        #is letter at same index the same?
+
+        #if guess is in correct position and correct place
         if answer[i] == guess[i]:
-            #append guessed letter to letters_guessed list
             letters_guessed.append(f"\033[1;30;42m{letter}\033[0m") # black on green background
-            #append to wordle, style green, add ("#")
+            # '#' is placeholder for letter 
             wordle.append(f"\033[1;32m#\033[0m")
-        #if letter is in answer
+
+        #if letter guessed is correct, but NOT in correct place
         elif letter in answer:
             letters_guessed.append(f"\033[1;30;43m{letter}\033[0m") # black on yellow background
+            # '*' letter guessed present in the chosen word, but not in the correct position
             wordle.append(f"\033[1;33m*\033[0m") # yellow
-        #if letter guessed 
+        
+        #if letter is INCORRECT
         else:
             letters_guessed.append(f"\033[1;30;47m{letter}\033[0m") # black on white background
             wordle.append(f"\033[1;31m.\033[0m") # red
@@ -37,13 +52,16 @@ def game(chosen_word):
 
     while not end_of_game:
         # ask user for input
+        # .strip() removes whitespace 
         guess = input(make_guess).strip().upper()
         # is a while loop that continues to run as long as the length of the guess variable is truthy
+        
+        #len() = return the length (the number of items) of an object
         while len(guess) != 5 or guess in already_guessed:
             if guess in already_guessed:
-                print("\033[1;31mYou've already guessed this word!!\033[0m")
+                print("\033[1;31mYou've already guessed this word!!\033[0m") #red
             else:
-                print("\033[1;31mPlease enter a 5-letter word!!\033[0m")
+                print("\033[1;31mPlease enter a 5-letter word!!\033[0m") #red
             guess = input(make_guess).strip().upper()
         #append to array 
         already_guessed.append(guess)
@@ -53,18 +71,22 @@ def game(chosen_word):
         full_wordle_pattern.append(pattern)
         
         #* s used to unpack the list of words guessed and pass each element as a separate argument to the print()
+        #sep by default is a single space, sep="\n" makes it =\n
         print(*all_words_guessed, sep="\n")
         if guess == chosen_word or len(already_guessed) == total_guesses:
             end_of_game = True
+
     if len(already_guessed) == total_guesses and guess != chosen_word:
-        print(f"\033[1;31mWORDLE X/{total_guesses}\033[0m")
-        print(f"\033[1;32mCorrect Word: {chosen_word}\033[0m")
+        print(f"\033[1;31mWORDLE X/{total_guesses}\033[0m") #red
+        print(f"\033[1;32mCorrect Word: {chosen_word}\033[0m") #green
     else:
-        print(f"\033[1;32mWORDLE {len(already_guessed)}/{total_guesses}\033[0m")
+    #{len(already_guessed)} = # player guesses
+    #{total_guesses} = max # of guesses 
+        print(f"\033[1;32mWORDLE {len(already_guessed)}/{total_guesses}\033[0m")#green
     print(*full_wordle_pattern, sep="\n")
 
 if __name__ == '__main__':
     print(start_game)
-    # choose a random word from the word list
+    #choice() ->  random element from the non-empty sequence
     chosen_word = choice(word_list).upper()
     game(chosen_word)
